0fb2f064279b92779fe2fa7cbf61e5df7f9831f4
bob <mangoraft@gmail.com>
2015-02-28 20:05:58 -0500

    removed seaport

--- bin/api.js
+++ bin/api.js
@@ -7,8 +7,8 @@ program.version(require('../package.json').version);
 
 program.description('View logs in teal-time.');
 
-program.option('-a, --addr [HOST]', 'Seaport HOST (default: 127.0.0.1)', '127.0.0.1');
-program.option('-p, --port [PORT]', 'Seaport  PORT (default: 9090)', 9090);
+program.option('-a, --addr [HOST]', 'HOST (default: 127.0.0.1)', '127.0.0.1');
+program.option('-p, --port [PORT]', 'PORT (default: 9090)', 9090);
 program.option('-r, --region [REGION]', 'Region located in (us)');
 program.option('-e, --environment [ENVIROMENT]', 'environment to use (services)');
 program.option('-n, --name [NAME]', 'name to use (port)');
@@ -18,15 +18,15 @@ program.option('-s, --secret [SECRET]', 'secret');
 program.parse(process.argv);
 
 var config = {
-	seaport : {
-		host : program.addr,
-		port : program.port
-	},
+	host : program.addr,
+	port : program.port,
 	multiTenant : program.multiTenant,
 	region : program.region,
 	environment : program.environment,
 	name : program.name,
 	secret : program.secret
 };
+
 var s = Server.createServer(config);
-s.listen(program.region + '-' + program.environment + '-' + program.name);
+
+s.listen();
